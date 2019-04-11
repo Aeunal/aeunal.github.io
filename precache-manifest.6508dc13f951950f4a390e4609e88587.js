c1b4650a27889bf0ba251a80fc4dd78ce2a6d0d5
Ahmet Emin Ünal <aeunal@hotmail.com>
2019-04-12 01:51:04 +0300

    Updates

---
+++ precache-manifest.6508dc13f951950f4a390e4609e88587.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "81efbb9b0ce030660fe0",
+    "url": "/static/js/main.07f62316.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "81efbb9b0ce030660fe0",
+    "url": "/static/css/main.b37711e5.chunk.css"
+  },
+  {
+    "revision": "c93a97af1170e68929c2275074f7a40b",
+    "url": "/index.html"
+  }
+];
